36dbdfa05cca13b32e04275cbc8e14bc8fa51b2a
Rajkawale7 <Rajkawale712@gmail.com>
2022-03-28 11:56:39 +0530

    User case 1 for Line comparison program

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,9 +1,34 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class LineComparison {
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Line comparison program");
+		System.out.println("**Welcome to The Line Comparison Computation Program**");
+		Scanner sc = new Scanner(System.in);
 		
+		//Taking Co-ordinates as a variables
+		int x1, y1, x2, y2;
+		
+		//Taking Inputs from users 
+		System.out.println("Enter the value of x1: ");
+		x1 = sc.nextInt();
+		
+		System.out.println("Enter the value of x2: ");
+		x2 = sc.nextInt();
+		
+		System.out.println("Enter the value of y1: ");
+		y1 = sc.nextInt();
+		
+		System.out.println("Enter the value of y2: ");
+		y2 = sc.nextInt();
+		
+		//Defining the Length of line
+		double length = Math.sqrt((x2 - x1)^2 + (y2 - y1)^2);
+		
+		System.out.println("The length of line is: " + length);
+		
+		
+	
 	}
-
 }
